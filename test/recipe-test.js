80347e8b72c656c68b43ea019deaa89fa06c0b94
alberto.eyo.contractor <alberto.eyo.contractor@bbva.com>
2017-04-12 11:20:01 +0200

    Incluimos los tests para contextos
    Incluimos tests funcionales y test unitarios para las necesidades de contextos

--- test/recipe-test.js
+++ test/recipe-test.js
@@ -3,15 +3,18 @@
 /* global define, it, describe, before */
 const assert = require('chai').assert;
 const exec = require('child_process').exec;
+const expect = require("chai").expect;
 
-describe('recipe pisco-sample validation', function() {
+describe('Testing the recipe functionality', function() {
     this.timeout(5000);
+    const REPORT_SPAIN = "Weather report: Madrid, Spain";
     it('Should \'recipe::weather\' works', (done) => {
         exec('node .. recipe::weather', {
             cwd: 'test'
         }, (error, stdout, stderr) => {
             assert.equal(stderr, '', stderr);
             assert.equal(error, null, error);
+            expect(stdout).contains(REPORT_SPAIN);
             done();
         });
     });
