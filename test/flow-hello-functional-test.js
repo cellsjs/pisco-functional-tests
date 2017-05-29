4f3a98342666c17c8fc549aedaa02178ab31b390
sbonacho <sbonacho@gmail.com>
2017-05-29 10:41:39 +0200

    feat(): delete emit test for plugins

--- test/flow-hello-functional-test.js
+++ test/flow-hello-functional-test.js
@@ -81,30 +81,13 @@ describe('Run the hello flow emitting for all steps', function() {
 });
 
 const PLUGIN_EMIT_1 = 'EMIT-plugin1';
-const PLUGIN_EMIT_2 = 'EMIT-plugin2';
 
 describe('Run the hello flow with emitting plugins', function() {
-  it(`Should return plugin-emitter-${PLUGIN_EMIT_1} emitted from emitter1 plugin`, function(done) {
+  it(`Should not return plugin-emitter-${PLUGIN_EMIT_1} emitted from emitter1 plugin`, function(done) {
     exec('node ' + process.env.PISCO + ' world:hello', {
       cwd: __dirname + '/world'
     }, (error, stdout, stderr) => {
-      expect(stdout).contain(`plugin-emitter-${PLUGIN_EMIT_1}`);
-      expectOkExecution(error, stdout, stderr, done);
-    });
-  });
-  it(`Should return plugin-emitter-2-${PLUGIN_EMIT_2} emitted from emitter2 plugin`, function(done) {
-    exec('node ' + process.env.PISCO + ' world:hello', {
-      cwd: __dirname + '/world'
-    }, (error, stdout, stderr) => {
-      expect(stdout).contain(`plugin-emitter-2-${PLUGIN_EMIT_2}`);
-      expectOkExecution(error, stdout, stderr, done);
-    });
-  });
-  it(`Should return plugin-emitter-2-All-${PLUGIN_EMIT_1} emitted from emitter1 plugin to all`, function(done) {
-    exec('node ' + process.env.PISCO + ' world:hello', {
-      cwd: __dirname + '/world'
-    }, (error, stdout, stderr) => {
-      expect(stdout).contain(`plugin-emitter-2-All-${PLUGIN_EMIT_1}`);
+      expect(stdout).not.contain(`plugin-emitter-${PLUGIN_EMIT_1}`);
       expectOkExecution(error, stdout, stderr, done);
     });
   });
