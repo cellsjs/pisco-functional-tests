da33bf539f2d2961a3049bc0d8268adec678b0a8
sbonacho <santiago.garcia-bonacho.contractor@bbva.com>
2017-05-30 15:55:20 +0200

    fix(): avoid piscosour fsUtils dependency

--- contexts/wonderfullWorld/index.js
+++ contexts/wonderfullWorld/index.js
@@ -2,10 +2,23 @@
 
 const path = require('path');
 const process = require('process');
-const fsUtils = require('piscosour/lib/utils/fsUtils');
 
 const _isWonderfullWorldFile = function() {
-  return fsUtils.exists('wonderfull.txt');
+
+  //TODO: change when testing framework exists
+  const exists = (filename) => {
+    try {
+      return fs.statSync(filename);
+    } catch (e) {
+      if (e.syscall === 'stat') {
+        return false;
+      } else {
+        throw e;
+      }
+    }
+  };
+
+  return exists('wonderfull.txt');
 };
 
 module.exports = {
